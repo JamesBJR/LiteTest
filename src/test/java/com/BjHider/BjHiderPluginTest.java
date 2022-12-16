335df7b64fa069dc18a2332d6d663a2e973b41bc
BJRtetris <jerobertsx93@gmail.com>
2022-12-16 13:08:44 -0500

    Added My code

--- src/test/java/com/BjHider/BjHiderPluginTest.java
+++ src/test/java/com/BjHider/BjHiderPluginTest.java
@@ -3,7 +3,7 @@ package com.BjHider;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class BjHiderPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
